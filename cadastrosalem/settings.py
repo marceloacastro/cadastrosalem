ad03a173df5e125cc77cdb431febef939cb54cdd
Marcelo de Arruda Castro <marceloacastro@yahoo.com.br>
2022-03-09 13:52:49 -0300

    Ajustes Deploy - Banco de Dado

--- cadastrosalem/settings.py
+++ cadastrosalem/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
-#import dj_database_url
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
